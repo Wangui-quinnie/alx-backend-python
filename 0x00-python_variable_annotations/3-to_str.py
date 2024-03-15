606d2f70e0ea9e1ad1cbb4ceb3cbf4b9a0c609f3
Wangui-quinnie <w.wangui@ymail.com>
2024-03-15 17:51:46 +0000

    Basic annotations - to string

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""A function to_str that takes a float n as argument and returns
+the string representationof the float."""
+
+
+def to_str(n: float) -> str:
+    """
+    Returns the string representation of a float.
+
+    Args:
+        n (float): The input float.
+
+    Returns:
+        str: The string representation of the input float.
+    """
+    return str(n)
