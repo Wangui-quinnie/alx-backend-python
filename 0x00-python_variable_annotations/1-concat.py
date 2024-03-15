5c8f15bf07398bd8f626bcb7d4dbf1b37d2482f9
Wangui-quinnie <w.wangui@ymail.com>
2024-03-15 17:23:06 +0000

    Basic annotations - concat

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""A function concat that returns a concatenated string."""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Concatenates two strings.
+
+    Args:
+        str1 (str): The first string.
+        str2 (str): The second string.
+
+    Returns:
+        str: The concatenated string.
+    """
+    return str1 + str2
