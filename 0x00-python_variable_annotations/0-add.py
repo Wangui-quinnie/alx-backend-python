5079555b0f27b3a7897ed6ffb8c6d0afc2f089f0
Wangui-quinnie <w.wangui@ymail.com>
2024-03-15 17:03:46 +0000

    Basic annotations - add

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""A function add that returns the sum of it arguments."""
+
+
+def add(a: float, b: float) -> float:
+    """
+    Adds two floats and returns their sum.
+
+    Args:
+        a (float): The first float.
+        b (float): The second float.
+
+    Returns:
+        float: The sum of a and b.
+    """
+    return a + b
