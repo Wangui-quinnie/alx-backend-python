911fcd2bf05c87ec8bc3f31f8996c947daf4697c
Wangui-quinnie <w.wangui@ymail.com>
2024-03-19 00:38:06 +0000

    corrected the logic

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -3,23 +3,10 @@
 Creates an asyncio.Task for the wait_n coroutine.
 """
 
-from typing import List
+from typing import List, Callable
 import asyncio
 
-wait_random = __import__('0-basic_async_syntax').wait_random
-
-
-async def wait_random_wrapper(max_delay: int) -> float:
-    """
-    Wrapper function for wait_random coroutine.
-
-    Args:
-        max_delay (int): The maximum delay in seconds.
-
-    Returns:
-        float: The random delay.
-    """
-    return await wait_random(max_delay)
+task_wait_random = __import__('3-tasks').task_wait_random
 
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
@@ -33,5 +20,7 @@ async def task_wait_n(n: int, max_delay: int) -> List[float]:
     Returns:
         List[float]: A list of delays.
     """
-    tasks = [wait_random_wrapper(max_delay) for _ in range(n)]
-    return await asyncio.gather(*tasks)
+    delays: List[float] = await asyncio.gather(
+        *(wait_random(max_delay) for _ in range(n))
+        )
+    return sorted(delays)
